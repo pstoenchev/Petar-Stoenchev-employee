61cafb88c5395a1297c2fd00c6373cac39919138
Petar Stoenchev <peter.stoenchev@abv.bg>
2021-01-26 13:02:33 +0200

    Initial Commit

---
+++ employee/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  employee
+//
+//  Created by Petar Stoenchev on 26.01.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
