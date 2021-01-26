34c90ba94e4df4fd1c62a7168326adec0ac38066
Petar Stoenchev <peter.stoenchev@abv.bg>
2021-01-26 16:34:33 +0200

    read from bundle and split array

--- employee/ViewController.swift
+++ employee/ViewController.swift
@@ -7,13 +7,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+
+// MARK: - Class Definition
+
+final class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        time()
         // Do any additional setup after loading the view.
     }
 
 
+  private func time() {
+        if let path = Bundle.main.path(forResource: "File", ofType: "txt") {
+            do {
+                let data = try String(contentsOfFile: path, encoding: .utf8)
+                let myString = data.components(separatedBy: ",")
+                    .compactMap { $0 }
+             
+                let first = myString[2]
+                let second = myString[3]
+                print(first)
+                print(second)
+        
+            } catch {
+            
+                print(error)
+            }
+        }
+
+    }
 }
 
